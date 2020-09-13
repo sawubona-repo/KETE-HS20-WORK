a194f47cc7ddc104ac2caeb5cd3d5c3210ea17a2
Tina-hslu <71207770+Tina-hslu@users.noreply.github.com>
2020-09-13 10:00:39 +0200

    Test Tina Messner

--- LB0-Kickoff/JS-Samples/p5js/sketch.js
+++ LB0-Kickoff/JS-Samples/p5js/sketch.js
@@ -1,20 +1,17 @@
 /* eslint-disable no-undef, no-unused-vars */
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // Put setup code here
 }
-
 function draw() {
   // Put drawings here
   if (mouseIsPressed) {
-    fill(45);
+    fill(90);
   } else {
     fill(215);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 180, 80);
 }
-
 // This Redraws the Canvas when resized
 windowResized = function () {
   resizeCanvas(windowWidth, windowHeight);
