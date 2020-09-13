ede0c8272689c4a61221536d8a6530519541131a
MichelCyril <71207054+MichelCyril@users.noreply.github.com>
2020-09-13 09:30:39 +0200

    add triangle

--- LB0-Kickoff/JS-Samples/p5js/sketch.js
+++ LB0-Kickoff/JS-Samples/p5js/sketch.js
@@ -9,10 +9,18 @@ function draw() {
   // Put drawings here
   if (mouseIsPressed) {
     fill(45);
+    triangle(
+      mouseX,
+      mouseY,
+      mouseX - 50,
+      mouseY - 50,
+      mouseX - 50,
+      mouseY + 50
+    );
   } else {
     fill(215);
+    ellipse(mouseX, mouseY, 80, 80);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
 
 // This Redraws the Canvas when resized
